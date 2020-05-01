026a5c520fb61081e1a379e6e7a07601f2603bc6
Dinh Phu <ndp17hp@gmail.com>
2020-05-01 17:43:21 +0700

    them file bai tap

--- tinh ngay tiep theo/src/NexDayCalculator.java
+++ tinh ngay tiep theo/src/NexDayCalculator.java
@@ -12,12 +12,20 @@ public class NexDayCalculator {
         LocalDateTime myLocalTime= LocalDateTime.of(myYear,myMonth,myDay,0,0,0);
         LocalDateTime nextLocalTime= myLocalTime.plusDays(1);
         Calendar myCld= Calendar.getInstance();
-        System.out.println(nextLocalTime.getYear() +" "+nextLocalTime.getMonthValue() + " "+nextLocalTime.getDayOfMonth());
         myCld.set(nextLocalTime.getYear(),nextLocalTime.getMonthValue()-1,nextLocalTime.getDayOfMonth());
-        Date trueDate = myCld.getTime();
         SimpleDateFormat simpleFormat= new SimpleDateFormat("YYYY-MM-dd");
-        String myDateString= simpleFormat.format(trueDate);
+        String myDateString=simpleFormat.format(myCld.getTime());
         return myDateString;
+//
+//        LocalDateTime myLocalTime= LocalDateTime.of(myYear,myMonth,myDay,0,0,0);
+//        LocalDateTime nextLocalTime= myLocalTime.plusDays(1);
+//        Calendar myCld= Calendar.getInstance();
+//        System.out.println(nextLocalTime.getYear() +" "+nextLocalTime.getMonthValue() + " "+nextLocalTime.getDayOfMonth());
+//        myCld.set(nextLocalTime.getYear(),nextLocalTime.getMonthValue()-1,nextLocalTime.getDayOfMonth());
+//        Date trueDate = myCld.getTime();
+//        SimpleDateFormat simpleFormat= new SimpleDateFormat("YYYY-MM-dd");
+//        String myDateString= simpleFormat.format(trueDate);
+        //return "myDateString";
     }
 
     public static void main(String[] args) {
